fb5311b7f47bf9fbfb0912d4e71824e6c9e1031c
ChristinRaju <2006christinraju@gmail.com>
2025-02-21 17:13:32 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import EmailAnalyzer from './components/EmailAnalyzer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <main>
+        <EmailAnalyzer />
+      </main>
+    </div>
+  );
+}
+
+export default App; 
