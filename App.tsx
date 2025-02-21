b9a14586d3e01b9528a748e6373032caf4caae21
ChristinRaju <2006christinraju@gmail.com>
2025-02-21 18:40:20 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import EmailAnalyzer from "./components/EmailAnalyzer";
 
 function App() {
   return (
-    <BrowserRouter basename="/Phishing-Email-Analyzer">
-      <div className="min-h-screen bg-gray-50">
-        <main>
-          <Routes>
-            <Route path="/" element={<EmailAnalyzer />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <Router basename="/Phishing-Email-Analyzer">
+      <Routes>
+        <Route path="/" element={<EmailAnalyzer />} />
+      </Routes>
+    </Router>
   );
 }
 
